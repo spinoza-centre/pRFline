20499d9d99c0cfa21ac5eedc84a26019d6c22cf3
gjheij <j.heij@spinozacentre.nl>
2022-07-19 15:03:18 +0200

    sync commands

--- scripts/line_fit.py
+++ scripts/line_fit.py
@@ -6,6 +6,7 @@
 
 import os
 import sys, getopt
+from bids import BIDSLayout
 from pRFline import fitting
 from linescanning.utils import get_file_from_substring
 import yaml
@@ -15,7 +16,8 @@ def main(argv):
 
     """line_fit.py
 
-    Preprocess and fit the pRF-data using the line-scanning acquisition. Assumes you have aligned your individual slices to one another and saved the transformation files as txt-files in the 'anat'-folder with your slices. If not, it'll throw an error. Basic procedures performed are:
+    Preprocess and fit the pRF-data using the line-scanning acquisition. Assumes you have aligned your individual slices to one another and saved the transformation files as txt-files in the 'anat'-folder with your slices. 
+    If not, it'll throw an error. Basic procedures performed are:
         - Highpass filtering [DCT]
         - aCompCor (if not enough voxels are present for PCA, the individual timecourses of the CSF/WM voxels themselves are used)
         - Lowpass filtering (if specified) [Savitsky-Golay]
@@ -25,23 +27,19 @@ def main(argv):
 
     Parameters
     ----------
-    -b <bids_dir>       path where 'func' and 'anat' folders live. Can be used instead of individually specifying '-f' and '-a'
-    -f <func_dir>       path to where the functional files live. We'll search for the "acq-3DEPI" 
-                        tag. If multiple files are found, we'll run preprocessing for them all.
-    -a <anat_dir>       path to where the the anatomical slices are. This can be used for aCompCor                       
-    -o <output_dir>     output directory + basename for output; some stuff about model type, stage, and parameter type will be appended
-    -l <log_dir>        directory that contains the "Screenshot"-directory to create the design matrix
-    -r <run ID>         run identifier in case we don't want to average across runs and fit the pRFs for individual runs
-    -t <trafo>          transformation file to warp the segmentations from ses-1 to current session
-    -i <iterations>     the experiment consists of one set of 8 sweeps (1 iteration) that can be repeated multiple times. We can average these iterations by specifying how many iterations we used. By default, the number of iterations will be read from the log-file from ExpTools, which will always be 1 given that we use the directory that has the screenshots. For that, we only need 1 iterations. With this extra flag you can specify how many iterations to consider.
-    -m <model>          Model type to use, e.g., "gauss" or "norm". Defaults to 'norm'.
-    --ses_trafo         transformation mapping ses-1 to the closest partial anatomy prior to the corresponding slice of the line-scanning acquisition
-    --filter_pca        cutoff frequency for highpass filtering of PCA components. This procedure can ensure that task-related frequencies in the PCA-components do not get removed
-    --rsq               r2-threshold to use during pRF-fitting. Parameters of voxels < threshold will be set to 0
-    -v                  turn on verbose
-    -g                  run model fitter with gray matter voxels only (based on the average tissue probabilities across runs)
-    -n                  don't do lowpass filtering on the timecourses
-    -q                  do quality control, not full fitting; plots will be stored in the 'anat' folder.
+    -b|--bids_dir   <bids_dir>      path where 'func' and 'anat' folders live
+    -o|--output_dir <output_dir>    output directory + basename for output; some stuff about model type, stage, and parameter type will be appended
+    -l|--log_dir    <log_dir>       directory that contains the "Screenshot"-directory to create the design matrix
+    -r|--run        <run ID>        run identifier in case we don't want to average across runs and fit the pRFs for individual runs
+    -i|--n_iter     <iterations>    the experiment consists of one set of 8 sweeps (1 iteration) that can be repeated multiple times. We can average these iterations by specifying how many iterations we used. By default, the number of iterations will be read from the log-file from ExpTools, which will always be 1 given that we use the directory that has the screenshots. For that, we only need 1 iterations. With this extra flag you can specify how many iterations to consider.
+    -m|--model      <model>         Model type to use, e.g., "gauss" or "norm". Defaults to 'norm'.
+    -v|--verbose                    turn on verbose
+    --ses_trafo     <file>          transformation mapping ses-1 to the closest partial anatomy prior to the corresponding slice of the line-scanning acquisition
+    --filter_pca    <float>         cutoff frequency for highpass filtering of PCA components. This procedure can ensure that task-related frequencies in the PCA-components do not get removed
+    --rsq           <float>         r2-threshold to use during pRF-fitting. Parameters of voxels < threshold will be set to 0
+    --hrf                           fit the HRF with the pRF-parameters as implemented in `prfpy`    
+    -g                              run model fitter with gray matter voxels only (based on the average tissue probabilities across runs)
+    -q                              do quality control, not full fitting; plots will be stored in the 'anat' folder.
 
     Returns
     ----------
@@ -55,8 +53,8 @@ def main(argv):
     >>> iters=2
     >>> filter_pca=0.18
     >>> bids_dir=${DIR_DATA_HOME}/sub-${sub}/ses-${ses}
-    >>> out_dir=${DIR_DATA_DERIV}/prf/sub-${sub}/ses-${ses}/sub-${sub}_ses-${ses}
-    >>> log_dir=/data1/projects/MicroFunc/Jurjen/programs/project_repos/LineExps/lineprf/logs/sub-${sub}_ses-${ses}_task-pRF_run-imgs
+    >>> out_dir=${DIR_DATA_DERIV}/prf/sub-${sub}/ses-${ses}/sub-${sub}_ses-${ses}_task-pRF
+    >>> log_dir=log_dir=${DIR_DATA_SOURCE}/sub-${sub}/ses-${ses}/sub-${sub}_ses-${ses}_task-pRF_run-imgs
     >>> ses_trafo=${DIR_DATA_DERIV}/pycortex/sub-${sub}/transforms/sub-${sub}_from-ses1_to-ses${ses}_rec-motion1_desc-genaff.mat
     >>> #
     >>> # run locally, filter PCA-components and verbose
@@ -69,28 +67,31 @@ def main(argv):
     >>> python line_fit.py -b ${bids_dir} -o ${out_dir} -l ${log_dir} --ses_trafo ${ses_trafo} -i ${iters} --filter_pca ${filter_pca} -v -q
     >>> #
     >>> # submit to cluster
-    >>> qsub -N prf_${sub} -pe smp 1 -wd /data1/projects/MicroFunc/Jurjen/programs/project_repos/pRFline/logs line_fit.py -b ${bids_dir} -o ${out_dir} -l ${log_dir} --ses_trafo ${ses_trafo} -i ${iters} --filter_pca ${filter_pca} -v -g
+    >>> qsub -N prf_${sub} -pe smp 1 -wd /data1/projects/MicroFunc/Jurjen/programs/project_repos/pRFline/logs line_fit.py -b ${bids_dir} -o ${out_dir} -l ${log_dir} --ses_trafo ${ses_trafo} -i ${iters} --filter_pca ${filter_pca} -v
     """
 
-    func_dir    = None
-    anat_dir    = None
-    bids_dir    = None
-    run_id      = None
-    output_dir  = None
-    log_dir     = None
-    verbose     = False
-    model       = "norm"
-    ses_trafo   = None
-    run_trafos  = None
-    n_iter      = None
-    filter_pca  = 0.2
-    rsq_thresh  = 0.05
-    lowpass     = True
-    qa          = False
-    gm_only     = False
+    bids_dir        = None
+    run_id          = None
+    output_dir      = None
+    log_dir         = None
+    verbose         = False
+    model           = "norm"
+    ses_trafo       = None
+    run_trafos      = None
+    n_iter          = None
+    filter_pca      = 0.2
+    rsq_thresh      = 0.05
+    qa              = False
+    gm_only         = False
+    fit_hrf         = False
+    filter_strat    = "hp"
+    make_report     = True
+    do_acompcor     = True
+    n_pix           = 100
 
+    # long options without argument: https://stackoverflow.com/a/54170513
     try:
-        opts = getopt.getopt(argv,"nqgvh:a:b:f:d:r:o:l:i:",["func_dir=", "bids_dir=", "n_iter=", "ses_trafo=", "anat_dir=", "output_dir=", "log_dir=", "filter_pca=", "rsq=", "run="])[0]
+        opts = getopt.getopt(argv,"nqgvh:b:d:r:o:f:l:i:",["bids_dir=", "n_iter=", "lowpass", "ses_trafo=", "output_dir=", "log_dir=", "filter_pca=", "rsq=", "run=", "hrf", "no_report", "verbose", "no_acompcor", "qa", "n_pix="])[0]
     except getopt.GetoptError:
         print("ERROR while handling arguments.. Did you specify an 'illegal' argument..?")
         print(main.__doc__)
@@ -101,11 +102,7 @@ def main(argv):
             print(main.__doc__)
             sys.exit()
         elif opt in ("-b", "--bids_dir"):
-            bids_dir = arg            
-        elif opt in ("-f", "--func_dir"):
-            func_dir = arg
-        elif opt in ("-a", "--anat_dir"):
-            anat_dir = arg
+            bids_dir = arg
         elif opt in ("-o", "--output_dir"):
             output_dir = arg
         elif opt in ("-l", "--log_dir"):
@@ -115,21 +112,29 @@ def main(argv):
         elif opt in ("-m", "--model"):
             model = arg            
         elif opt in ("-i", "--n_iter"):
-            n_iter = arg            
+            n_iter = arg        
         elif opt in ("--ses_trafo"):
             ses_trafo = arg
+        elif opt in ("-n", "--lowpass"):
+            filter_strat = "lp"
         elif opt in ("--filter_pca"):
             filter_pca = float(arg)
         elif opt in ("--rsq"):
             rsq_thresh = arg
-        elif opt in ("-n"):
-            lowpass = False                    
-        elif opt in ("-v"):
+        elif opt in ("--hrf"):
+            fit_hrf = True                 
+        elif opt in ("-v", "--verbose"):
             verbose = True
         elif opt in ("-g"):
             gm_only = True
-        elif opt in ("-q"):
-            qa = True                 
+        elif opt in ("-q", "--qa"):
+            qa = True
+        elif opt in ("--no_report"):
+            make_report = False
+        elif opt in ("--no_acompcor"):
+            do_acompcor = False
+        elif opt in ("--n_pix"):
+            n_pix = arg
     
     if len(argv) == 0:
         print(main.__doc__)
@@ -138,17 +143,21 @@ def main(argv):
     #---------------------------------------------------------------------------------------
     # Error handling of mandatory arguments
     if bids_dir == None:
-        if func_dir == None:
-            raise ValueError("Please specify the path to the functional files")
-    else:
-        func_dir = opj(bids_dir, 'func')
-        anat_dir = opj(bids_dir, 'anat')
+        raise ValueError("Please specify the path to the functional files")
 
     if output_dir == None:
         raise ValueError("Please specify an output directory (e.g., derivatives/prf/<subject>/<ses-)")
 
     if log_dir == None:
         raise ValueError("Please specify a log-directory with the Screenshot-directory")
+
+    # get layout
+    layout = BIDSLayout(bids_dir, validate=False)
+    anats = layout.get(datatype="anat", return_type="file")
+    funcs = layout.get(datatype="func", return_type="file")
+
+    # get run-to-run transformation files
+    run_trafos = layout.get(datatype="anat", extension=['txt'], return_type="file")
 
     #---------------------------------------------------------------------------------------
     # check if we should do single runs or all runs at once for the output name of aCompCor figure
@@ -159,8 +168,18 @@ def main(argv):
         func_search = ["task-pRF", "bold.mat"]
         anat_search = ["acq-1slice", ".nii.gz"]
 
-    func_files = get_file_from_substring(func_search, func_dir)
-    ref_slices = get_file_from_substring(anat_search, anat_dir)
+    func_files = get_file_from_substring(func_search, funcs, exclude="acq-3DEPI")
+    ref_slices = get_file_from_substring(anat_search, anats)
+
+    # check length if lists
+    if len(func_files) != len(ref_slices):
+        raise ValueError(f"number of func files ({len(func_files)}) does not match number of anatomical files ({len(ref_slices)})")
+
+    # check length if lists
+    if do_acompcor:
+        if len(ref_slices) != 0:
+            if len(run_trafos) != len(ref_slices):
+                raise ValueError(f"number of transformation files ({len(run_trafos)}) does not match number of anatomical files ({len(ref_slices)})")
 
     #---------------------------------------------------------------------------------------
     # read some settings from the log-file
@@ -170,7 +189,6 @@ def main(argv):
 
     # reconstruct design in LineExps/lineprf/session.py
     design          = settings['design']
-    bsl_trials      = int(design.get('start_duration')/design.get('stim_duration'))
     sweep_trials    = int(design.get('bar_steps')*2 + (design.get('inter_sweep_blank')//design.get('stim_duration')))
     rest_trials     = int(design.get('blank_duration')//design.get('stim_duration'))
     block_trials    = int(sweep_trials*2 + rest_trials)
@@ -186,36 +204,26 @@ def main(argv):
         n_iter = int(n_iter)
 
     # check if we should skip the baseline; should be skipped if n_iter > 1 because of weird artifacts of averaging
+    strip_baseline = False
     if n_iter > 1:
         strip_baseline = True
-    else:
-        strip_baseline = False
 
-    #---------------------------------------------------------------------------------------
-    # check if we got anatomies + trafo files
-    if anat_dir != None:
-        if ses_trafo == None:
-            raise ValueError("Need a transformation file mapping ses-1 to current session")
-
-        if isinstance(func_files, list):
-            try:
-                run_trafos = get_file_from_substring(".txt", anat_dir)
-            except:
-                raise ValueError(f"Received multiple functional files and an anatomical folder, but no run-to-run registration files. Manually align the slices and save them as txt-files in {anat_dir}")
-    
     #---------------------------------------------------------------------------------------
     # set output basename
     if run_id != None:
         output_base_prf = os.path.basename(output_dir)+f"_run-{run_id}"
     else:
-        output_base_prf = os.path.basename(output_dir)
+        if len(func_files) > 1:
+            output_base_prf = os.path.basename(output_dir)+f"_run-avg"
+        else:
+            output_base_prf = os.path.basename(output_dir)
 
     #---------------------------------------------------------------------------------------
     # initiate model fitting
     model_fit = fitting.FitLines(func_files=func_files,
                                  TR=t_r,
-                                 low_pass=lowpass,
-                                 window_size=19,
+                                 filter_strategy=filter_strat,
+                                 window_size=11,
                                  log_dir=log_dir,
                                  stage='grid+iter',
                                  model=model,
@@ -224,7 +232,7 @@ def main(argv):
                                  n_iterations=n_iter,
                                  verbose=verbose,
                                  strip_baseline=strip_baseline,
-                                 acompcor=True,
+                                 acompcor=do_acompcor,
                                  ref_slice=ref_slices,
                                  filter_pca=filter_pca,
                                  rsq_threshold=rsq_thresh,
@@ -232,9 +240,11 @@ def main(argv):
                                  run_2_run=run_trafos,
                                  output_dir=os.path.dirname(output_dir),
                                  output_base=output_base_prf,
-                                 save_as=opj(anat_dir, os.path.basename(output_dir)),
                                  voxel_cutoff=300,
-                                 ribbon=gm_only)
+                                 ribbon=gm_only,
+                                 fit_hrf=fit_hrf,
+                                 report=make_report,
+                                 n_pix=n_pix)
 
     # fit
     if not qa:
--- scripts/partial_fit.py
+++ scripts/partial_fit.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
+#$ -q long.q@jupiter
 #$ -cwd
 #$ -j Y
 #$ -V
 
 import os
 import sys, getopt
-from pRFline import utils, fitting
-from linescanning.utils import get_file_from_substring
+from pRFline import fitting
+from linescanning import utils
+from bids import BIDSLayout
 opj = os.path.join
 
 def main(argv):
@@ -18,14 +20,21 @@ def main(argv):
 
     Parameters
     ----------
+    -b <bids_dir>       path pointing to derivatives-folder containing the input from `fMRIPrep` and the 
+                        output for pRFs. Overrules <func_dir> and <output_dir>
     -f <func_dir>       path to where the functional files live. We'll search for the "acq-3DEPI" 
                         tag. If multiple files are found, we'll run preprocessing for them all.
     -o <output_dir>     output directory; should be project root directory with <subject>/<ses-X>/
                         func.
     -l <log_dir>        directory that contains the "Screenshot"-directory to create the design matrix
-    -v                  turn on verbose
-    -g                  run model fitter with 'gauss' model instead of 'norm'. Will do grid+iter fit
+    --tr                set manual TR in seconds [default = 1.111s]
+    -v|--verbose        turn on verbose
+    -g|--gauss          run model fitter with 'gauss' model instead of 'norm'. Will do grid+iter fit
                         regardless of model choice
+    --psc               use percent signal change as standardization [default]
+    --zscore            use zscore as standardization
+    --fsnative          fit in FSNative space
+    --fsaverage         fit in FSAverage space
 
     Returns
     ----------
@@ -33,19 +42,27 @@ def main(argv):
 
     Example
     ----------
-    >>> python partial_fit.py -f /mnt/export/data1/projects/MicroFunc/Jurjen/projects/hemifield/testing/preprocess_func -o /mnt/export/data1/projects/MicroFunc/Jurjen/projects/hemifield/testing/preprocess_func -l /data1/projects/MicroFunc/Jurjen/projects/hemifield/testing/prf_design/sub-003_ses-0_task-pRF_run-0 -v
-
-    >>> qsub -N prf_003 -pe smp 1 -wd /data1/projects/MicroFunc/Jurjen/programs/project_repos/pRFline/logs partial_fit.py -f /mnt/export/data1/projects/MicroFunc/Jurjen/projects/hemifield/testing/preprocess_func -o /mnt/export/data1/projects/MicroFunc/Jurjen/projects/hemifield/testing/preprocess_func -l /data1/projects/MicroFunc/Jurjen/projects/hemifield/testing/prf_design/sub-003_ses-0_task-pRF_run-0 -v
+    >>> 
     """
 
-    func_dir    = None
-    output_dir  = None
-    log_dir     = None
-    verbose     = False
-    model       = "norm"
+    subject         = None
+    session         = None
+    bids_dir        = None
+    func_dir        = None
+    output_dir      = None
+    log_dir         = None
+    verbose         = False
+    model           = "norm"
+    fit_hrf         = False
+    standardization = "psc"
+    space           = 'func'
+    TR              = 1.111
+    rsq_thresh      = 0.05
+    n_pix           = 100
+    fit             = True
 
     try:
-        opts = getopt.getopt(argv,"gvh:n:f:d:o:l:",["func_dir=", "output_dir=", "log_dir="])[0]
+        opts = getopt.getopt(argv,"gvh:b:s:n:f:d:o:l:",["bids_dir=", "subject=", "session=", "func_dir=", "output_dir=", "log_dir=", "tr=", "hrf", "verbose", "fsnative", "fsaverage", "rsq=", "n_pix=", "no-fit"])[0]
     except getopt.GetoptError:
         print("ERROR while handling arguments.. Did you specify an 'illegal' argument..?")
         print(main.__doc__)
@@ -55,37 +72,111 @@ def main(argv):
         if opt == '-h':
             print(main.__doc__)
             sys.exit()
+        elif opt in ("-s", "--subject"):
+            subject = arg
+        elif opt in ("-n", "--session"):
+            session = arg            
+        elif opt in ("-b", "--session"):
+            bids_dir = arg            
+        elif opt in ("-b", "--bids_dir"):
+            bids_dir = arg           
         elif opt in ("-f", "--func_dir"):
             func_dir = arg
         elif opt in ("-o", "--output_dir"):
             output_dir = arg
         elif opt in ("-l", "--log_dir"):
             log_dir = arg
-        elif opt in ("-v"):
+        elif opt in ("-v", "--verbose"):
             verbose = True
-        elif opt in ("-g"):
+        elif opt in ("-g", "--gauss"):
             model = "gauss"     
+        elif opt in ("--zscore"):
+            standardization = "zscore"
+        elif opt in ("--psc"):
+            standardization = "psc"
+        elif opt in ("--fsaverage"):
+            space = "fsaverage"
+        elif opt in ("--fsnative"):
+            space = "fsnative"     
+        elif opt in ("--tr"):
+            TR = float(arg)           
+        elif opt in ("--rsq"):
+            rsq_thresh = arg
+        elif opt in ("--n_pix"):
+            n_pix = arg   
+        elif opt in ("--no-fit"):
+            fit = False
 
-    # this is a bit more informative than if len(argv) < 8..
-    if func_dir == None:
-        raise ValueError("Please specify the path to the functional files")
+    if len(argv) == 0:
+        print(main.__doc__)
+        sys.exit(1)
+    
+    # check first if we got a BIDS-directory. If not, check -f and -o flag
+    if bids_dir == None:
+        if func_dir == None:
+            raise ValueError("Please specify the path to the functional files")
 
-    if output_dir == None:
-        raise ValueError("Please specify an output directory (e.g., derivatives/prf/<subject>/<ses-)")
+        if output_dir == None:
+            raise ValueError("Please specify an output directory (e.g., derivatives/prf/<subject>/<ses-)")
+
+        # look for files within func_dir. Ensures compatibility with BIDS-option below
+        file_list = func_dir
+    else:
+        
+        if subject == None:
+            raise ValueError("Must have subject ID (only digits) for this option")
+
+        if verbose:
+            print(f"Reading BIDS-directory '{bids_dir}'")
+
+        func_dir = bids_dir
+        layout = BIDSLayout(func_dir, validate=False)
+        file_list = layout.get(subject=subject, session=session, return_type='file')
+
+        if verbose:
+            print(f"BIDS layout: {layout}")
+        
+        # set output directory
+        if session != None:
+            base_dir = opj(f'sub-{subject}', f'ses-{session}')
+        else:
+            base_dir = f'sub-{subject}'
+
+        # combine base with directory 
+        output_dir = opj(os.path.dirname(bids_dir), 'prf', base_dir)
 
     if log_dir == None:
         raise ValueError("Please specify a log-directory with the Screenshot-directory")
 
-    func_files = get_file_from_substring(["hemi-LR", "bold.npy"], func_dir)
+    # make directory
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+
+    # fetch files depending on space
+    if space == "func":
+        # try to look for brain extracted file
+        try:
+            func_files = utils.get_file_from_substring(["masked_bold.nii.gz"], file_list, exclude="space-")
+        except:
+            func_files = utils.get_file_from_substring(["preproc_bold.nii.gz"], file_list, exclude="space-")
+    else:
+        func_files = utils.get_file_from_substring([f"space-{space}", "hemi-LR", "bold.func.npy"], file_list)
+
     model_fit = fitting.FitPartialFOV(func_files=func_files,
                                       output_dir=output_dir,
-                                      TR=1.111,
+                                      TR=TR,
                                       log_dir=log_dir,
                                       stage='grid+iter',
                                       model=model,
-                                      verbose=verbose)
+                                      verbose=verbose,
+                                      fit_hrf=fit_hrf,
+                                      standardization=standardization,
+                                      rsq_threshold=rsq_thresh,
+                                      n_pix=n_pix)
 
-    model_fit.fit()
+    # fit
+    if fit:
+        model_fit.fit()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
