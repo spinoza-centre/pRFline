c78b3a386a3ac0bfdb095f8a19909230bd6e93e4
gjheij <j.heij@spinozacentre.nl>
2023-01-31 18:10:33 +0100

    create distance on surface plot

--- scripts/dist_on_surf.py
+++ scripts/dist_on_surf.py
@@ -56,9 +56,11 @@ Example
     n_iter = 1
     kernel = 1
     smooth = False
+    full_plot = False
+    overwrite = False
 
     try:
-        opts = getopt.getopt(argv,"hm:v:k:i:",["help", "subject=", "session=", "no_verbose", "norm", "gauss", "dog", "css", "lp", "lh", "rh", "webshow", "sm", "smooth", "kernel", "iter", "no_verbose"])[0]
+        opts = getopt.getopt(argv,"hm:v:k:i:o:",["help", "subject=", "session=", "no_verbose", "norm", "gauss", "dog", "css", "lp", "lh", "rh", "webshow", "sm", "smooth", "kernel", "iter", "no_verbose", "plot", "ow", "overwite"])[0]
     except getopt.GetoptError:
         print("ERROR while handling arguments.. Did you specify an 'illegal' argument..?", flush=True)
         print(main.__doc__, flush=True)
@@ -95,8 +97,11 @@ Example
         elif opt in ("--sm", "--smooth"):
             smooth = True
         elif opt in ("--no_verbose"):
-            verbose = False            
-        
+            verbose = False
+        elif opt in ("--plot"):
+            full_plot = True          
+        elif opt in ("--overwrite", "-o", "--ow"):
+            overwrite = True            
 
     utils.verbose("\ndist_on_surf.py", verbose)
 
@@ -118,17 +123,35 @@ Example
     else:
         process_list = [subject]
 
+    # initiate dictionary
     distsurf = {}
     for ii in ["subject","geodesic","euclidian","dva"]:
         distsurf[ii] = []
 
-    for subject in process_list:
+    if full_plot:
+        import imageio
+        import matplotlib.pyplot as plt
+        import seaborn as sns
+        
+        cmap_subj = "Set2"
+        fig,axs = plt.subplots(ncols=len(process_list), figsize=(30,5))
+        fig2,axs2 = plt.subplots(ncols=len(process_list), figsize=(30,5))
+        sub_colors = sns.color_palette(cmap_subj, len(process_list))
+
+    for ix,subject in enumerate(process_list):
 
         utils.verbose(f"\n**************************************** Processing {subject} ***************************************", verbose)
         
         # read subject-specific session from dictionary
         ses = subj_obj.get_session(subject)
-        data_output = opj(design_dir, subject)
+
+        # set basename for output
+        if smooth:
+            add_str = f"smooth-true_kernel-{kernel}_iter-{n_iter}"
+        else:
+            add_str = "smooth-false"
+
+        base = f"{subject}_model-{model}_{add_str}"
         
         # prf directory
         prf_dir = opj(
@@ -147,7 +170,10 @@ Example
             use_md = "gauss"
 
         # find EPI estimates
-        prf_epi = opj(prf_dir, "ses-1", f"{subject}_ses-1_task-2R_roi-V1_model-{use_md}_stage-iter_desc-prf_params.pkl")
+        prf_epi = opj(
+            prf_dir, 
+            "ses-1", 
+            f"{subject}_ses-1_task-2R_roi-V1_model-{use_md}_stage-iter_desc-prf_params.pkl")
 
         # put everything in class
         if os.path.exists(prf_line) and os.path.exists(prf_epi):
@@ -177,39 +203,90 @@ Example
             distsurf["euclidian"].append(sg.dist_euclidian)
             distsurf["dva"].append(sg.dva_dist)
 
-            if webshow:
+            if webshow or full_plot:
                 # make the vertex objects
                 sg.make_vertex(one_hemi=True)
 
                 # create data dict
                 data_d = {
                     "distance": sg.dist_v,
-                    "targ-match": sg.target_matched_v
+                    "targ-match": sg.target_matched_v,
+                    "curvature": sg.curv_v
                 }
 
-                # initialize saving object (also opens the webviewers)
-                pyc_save = pycortex.SavePycortexViews(
-                    data_d,
-                    subject=subject,
-                    fig_dir=opj(fig_dir,subject),
-                    zoom=True)
+                # save image if not exists or if overwrite==True
+                data_keys = list(data_d.keys())
+                fn_image = opj(fig_dir, subject, f"{base}_desc-{data_keys[0]}.png")
+                fn_image2 = opj(fig_dir, subject, f"{base}_desc-{data_keys[1]}.png")
 
-                time.sleep(3)
-                pyc_save.save_all()
+                if not os.path.exists(fn_image) or overwrite or webshow:
+                    # initialize saving object (also opens the webviewers)
+                    pyc_save = pycortex.SavePycortexViews(
+                        data_d,
+                        subject=subject,
+                        fig_dir=opj(fig_dir,subject),
+                        zoom=True,
+                        base_name=base)
+
+                    if not os.path.exists(fn_image) or overwrite:
+                        time.sleep(5)
+                        pyc_save.save_all()
+                
+                if full_plot:
+                    for xx,img in enumerate([fn_image,fn_image2]):
+                        if xx == 0:
+                            ax = axs[ix]
+                        else:
+                            ax = axs2[ix]
+
+                        # add image to axis
+                        im = imageio.imread(img)
+                        ax.imshow(im)
+                        ax.set_title(
+                            subject, 
+                            fontsize=18, 
+                            fontname="Montserrat",
+                            color=sub_colors[ix], 
+                            fontweight="bold")
+
+                        ax.annotate(
+                            f"{round(sg.dist_euclidian,2)}mm", 
+                            (0.5,0), 
+                            va="center",
+                            ha="center",
+                            fontsize=14, 
+                            fontname="Montserrat",
+                            xycoords="axes fraction")
+
+                        ax.axis('off')                    
+
         else:
             raise FileNotFoundError(f"Could not find '{prf_line}' or '{prf_epi}'")
 
+    base = f"sub-all_model-{model}_{add_str}"
     if len(distsurf) > 0:
-        if smooth:
-            add_str = f"smooth-true_kernel-{kernel}_iter-{n_iter}"
-        else:
-            add_str = "smooth-false"
-
-        fname = opj(fig_dir, f"model-{model}_{add_str}_desc-dist_on_surf.csv")
+        fname = opj(fig_dir, f"{base}_desc-dist_on_surf.csv")
         utils.verbose(f"Writing {fname}", verbose)
         df_surf = pd.DataFrame(distsurf)
         df_surf["ix"] = 0
         df_surf.to_csv(fname)
+
+    if full_plot:
+        for ix,img in enumerate(["dist_on_surf","targ_on_surf"]):
+            if ix == 0:
+                ff = fig
+            else:
+                ff = fig2
+
+            fname = opj(fig_dir, f"{base}_desc-{img}")
+            for ext in ['png','svg']:
+                utils.verbose(f"Writing {fname}.{ext}", verbose)
+                ff.savefig(
+                    f"{fname}.{ext}",
+                    bbox_inches="tight",
+                    dpi=300,
+                    facecolor="white"
+                )
 
 if __name__ == "__main__":
     main(sys.argv[1:])
